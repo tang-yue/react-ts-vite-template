01741bbb8267efb3d0a53272925375235d1209b7
tang_yue1129@163.com <tangyue5@tal.com>
2023-03-31 16:06:41 +0800

    feat: add axios config

--- src/request/index.ts
+++ src/request/index.ts
@@ -1,10 +1,40 @@
-import { RawAxiosResponseHeaders, AxiosResponseHeaders, AxiosRequestConfig } from 'axios'
+import axios, { AxiosError, RawAxiosResponseHeaders, AxiosResponseHeaders } from 'axios'
+import type { AxiosInstance, AxiosResponse, AxiosRequestConfig } from 'axios'
 
-export interface AxiosResponse<T = any, D = any>  {
-  data: T;
-  status: number;
-  statusText: string;
-  headers: RawAxiosResponseHeaders | AxiosResponseHeaders;
-  config: AxiosRequestConfig<D>;
-  request?: any;
+const service: AxiosInstance = axios.create({
+  baseURL: '/api',
+  timeout: 30000
+})
+
+service.interceptors.request.use((config: AxiosRequestConfig) => {
+  config.headers
+  return config
+}, (error: AxiosError) => {
+  return Promise.reject(error)
+})
+
+service.interceptors.response.use((response: AxiosResponse) => {
+  if (response.data.code == 0) {
+    return response.data.data
+  } else {
+    // 处理错误
+  }
+}, (error: AxiosError) => {
+  // 根据 status 处理错误
+  return Promise.reject(error)
+})
+
+export const http = {
+  get<T=any>(url: string, config?: AxiosRequestConfig) : Promise<T> {
+    return service.get(url, config)
+  },
+  post<T=any>(url: string, data?: object, config?: AxiosRequestConfig) :Promise<T> {
+    return service.post(url, data, config)
+  },
+  put<T = any>(url: string, data?: object, config?: AxiosRequestConfig): Promise<T> {
+    return service.put(url, data, config)
+  },
+  delete<T=any>(url: string, config?: AxiosRequestConfig) : Promise<T> {
+    return service.delete(url, config)
+  }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import vitePluginImp from 'vite-plugin-imp'
+import gzipPlugin from 'rollup-plugin-gzip'
 import path from 'path' // 要引入 @types/node 包
 
 // https://vitejs.dev/config/
@@ -19,6 +20,23 @@ export default defineConfig((config) => {
         ]
       })
     ],
+    build: {
+      minify: 'terser',
+      rollupOptions: {
+        plugins: [gzipPlugin()]
+      },
+      terserOptions: {
+        /**
+          * mode 用来判断环境
+          */
+        // compress: {
+        //   // 默认是 false
+        //   drop_console: config.mode === 'prod',
+        //   // 默认是 true
+        //   drop_debugger: config.mode === 'prod',
+        // },
+      }
+    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, 'src')
